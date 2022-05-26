9bb5c81796d66f9d654325dfc3e557c10ebf6ba5
mingdebaba <51104897+mingdebaba@users.noreply.github.com>
2022-05-27 01:11:30 +0900

    none  form input

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='GET':
+        return 'this is get request'
+    else:
+        return 'this is post request'
+if __name__=='__main__':
+    app.run(debug=True)
 
 
 
